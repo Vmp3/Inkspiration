4a54fd16c75a5c5a1a3e95e2a31a6e9f5ef28195
PedrohFolster <pedrohfolster@hotmail.com>
2025-05-11 23:22:44 -0300

    Feature - Adicionando pagina de explorar e adicionando responsividade no sistema

--- frontend/App.js
+++ frontend/App.js
@@ -10,6 +10,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import MainScreen from './src/screens/MainScreen';
+import ExploreScreen from './src/screens/ExploreScreen';
 import toastConfig from './src/config/toastConfig';
 
 const Stack = createNativeStackNavigator();
@@ -29,6 +30,7 @@ export default function App() {
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="Explore" component={ExploreScreen} />
         </Stack.Navigator>
         <Toast ref={(ref) => Toast.setRef(ref)} config={toastConfig} />
       </NavigationContainer>
