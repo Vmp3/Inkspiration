4b7a4002f7b12154c11bb3b994300b6d45da3322
PedrohFolster <pedrohfolster@hotmail.com>
2025-07-02 01:13:20 -0300

    FIX - Corrigindo erros env

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -5,16 +5,14 @@ let BASE_URL = '';
 
 if (ENVIRONMENT === 'prod') {
   BASE_URL = PROD_IP;
-} else if (ENVIRONMENT === 'dev') {
-  if (Platform.OS === 'web') {
-    BASE_URL = WEB_API_URL || 'http://localhost:8080';
-  } else if (Platform.OS === 'android') {
-    BASE_URL = MOBILE_API_URL || 'http://10.0.2.2:8000';
-  } else {
-    BASE_URL = WEB_API_URL || 'http://localhost:8080';
-  }
 } else {
-  BASE_URL = 'http://localhost:8080';
+  if (Platform.OS === 'web') {
+    BASE_URL = WEB_API_URL;
+  } else if (Platform.OS === 'android') {
+    BASE_URL = MOBILE_API_URL;
+  } else {
+    BASE_URL = WEB_API_URL;
+  }
 }
 
 export const API_CONFIG = {
