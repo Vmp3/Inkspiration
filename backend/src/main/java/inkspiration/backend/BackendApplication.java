2347608bd31546f3d3c15fc6ac87bd4e7f676945
PedrohFolster <pedrohfolster@hotmail.com>
2025-07-06 23:52:40 -0300

    FIX - Corrigindo erro do ViaCEP

--- backend/src/main/java/inkspiration/backend/BackendApplication.java
+++ backend/src/main/java/inkspiration/backend/BackendApplication.java
@@ -9,6 +9,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// Configurações SSL para melhor compatibilidade
+		System.setProperty("https.protocols", "TLSv1.2,TLSv1.3");
+		System.setProperty("jdk.tls.client.protocols", "TLSv1.2,TLSv1.3");
+		System.setProperty("sun.security.ssl.allowUnsafeRenegotiation", "true");
+		System.setProperty("sun.security.ssl.allowLegacyHelloMessages", "true");
+		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
